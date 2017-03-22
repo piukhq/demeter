cabd0b6834a70163670c19116f38019ea4afab65
Chris Latham <cl@bink.com>
2017-03-22 11:29:50 +0000

    archive after sending to visa, instead of before

--- visa_enrol.bash
+++ visa_enrol.bash
@@ -16,12 +16,12 @@ archive_dir="/data/demeter/archive/visa"
 archive_host="txmatch@172.26.127.22"
 archive_port="2020"
 
-log "[$(date)] Archiving $file_name"
-log $(/usr/bin/rsync -e "ssh -p $archive_port" -a --progress "$file_path" "$archive_host:$archive_dir")
-
 # lftp requires that you provide a password even if it's not going to be used.
 # it checks password first, ssh agent second.
 log "[$(date)] Transferring $file_name to Visa"
 log $(/usr/bin/lftp 'sftp://la.taqc:DUMMY_PASSWORD@198.241.159.22' -e "set sftp:connect-program 'ssh -a -x -i /root/.ssh/id_rsa_visa'; put $file_path; bye")
+
+log "[$(date)] Archiving $file_name"
+log $(/usr/bin/rsync -e "ssh -p $archive_port" -a --progress --remove-source-files "$file_path" "$archive_host:$archive_dir")
 
 log "[$(date)] $file_name transfer successful"
