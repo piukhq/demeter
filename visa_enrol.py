c3f33ca04a44c84335883a702355b6950b928761
Chris Latham <cl@bink.com>
2017-03-23 16:06:03 +0000

    add regex to validate enrolment file names in visa enrol script

--- visa_enrol.py
+++ visa_enrol.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
 from sys import argv
 import logging
+import re
+import os
 
 from plumbum.cmd import lftp, rsync
 
 
-logfile = '/var/log/demeter/visa-enrol.log'
+logfile = '/var/log/demeter/enrolment.log'
 
 _, file_path = argv
+file_name = os.path.basename(file_path)
 
 archive_server = {
     'host': 'txmatch@172.26.127.22',
     'port': '2020',
-    'dir': '/data/demeter/archive/amex/',
+    'dir': '/data/demeter/archive/visa/',
 }
+
+enrolment_file_name_pattern = re.compile('LOYANG_REG_PAN_\d+\.gpg')
 
 logger = logging.getLogger('__name__')
 logger.basicConfig(filename=logfile,
@@ -21,6 +26,11 @@ logger.basicConfig(filename=logfile,
                    format='[%(asctime)s] %(levelname)s :: %(message)s')
 
 logger.debug('Running visa enrolment file export process for file: {}.'.format(file_path))
+
+if not enrolment_file_name_pattern.match(file_name):
+    logger.debug('Skipping file name {} as it does not match the regex {}'.format(
+        file_name, enrolment_file_name_pattern.pattern))
+    exit(0)
 
 logger.debug('Transferring file to visa...')
 lftp('sftp://la.taqc:DUMMY_PASSWORD@198.241.159.22',
